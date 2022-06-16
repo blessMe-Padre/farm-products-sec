110af2dd479da1d58b8dcfc5e18130d4a3d65df1
Сергей Субботин <zarodiny@yandex.ru>
2022-06-16 18:26:59 +1000

    добавляет чекбоксы

---
+++ src/components/ui/checkbox/checkbox.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Label } from "./../../styled/label";
+import { VisuallyHiddenInput } from "./../../styled/visually-hidden-input";
+
+// чекбокс
+function Checkbox({
+    onClick,
+    labelComponent, // Компонент для отображения label
+    isChecked, // выбранно ли значение
+    name, // имя
+    value, // значение
+    text, // текст элемента
+    onChange, // событие при изменении
+    ...props
+}) {
+    const LabelComponent = labelComponent;
+
+    return (
+        <Label>
+            <VisuallyHiddenInput
+                value={value}
+                checked={isChecked}
+                name={name}
+                onChange={() => onChange(value)}
+                {...props}
+                type="checkbox"
+            />
+            <LabelComponent onClick={() => onClick(value)} $isChecked={isChecked}>
+                {text}
+            </LabelComponent>
+        </Label>
+    );
+}
+
+export default Checkbox;
--- src/pages/order/Order.jsx
+++ src/pages/order/Order.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from '../../components/Container';
 
 import Panel from '../../components/ui/panel/panel';
@@ -11,22 +11,36 @@ import {
     StyledOrder,
     PriceLabel,
     PriceValue,
+    CheckboxLabel
 } from './styles';
 import Button from '../../components/ui/buttons/Button';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import SwiperCore, { Pagination, Mousewheel, Scrollbar } from "swiper/core";
+import CheckboxList from "../../components/ui/checkbox-list/checkbox-list";
 
 SwiperCore.use([Mousewheel, Pagination, Scrollbar]);
 
 const Order = ({ products }) => {
+    const [selectProductIds, setSelectProductIds] = useState([]);
     return (
         <Container >
             <StyledOrder as='form'>
                 <Column>
                     <Panel>
                         <Title as="h2" size={TitleSize.EXTRA_SMALL} marginBottom={12}>Выберите продукты</Title>
+                        <CheckboxList
+                            labelComponent={CheckboxLabel}
+                            name={"select-products"}
+                            isGridList={false}
+                            options={products.map((product) => ({
+                                value: product.id,
+                                title: product.name
+                            }))}
+                            selectValues={selectProductIds}
+                            onChange={setSelectProductIds}
+                        />
                     </Panel>
                     <Panel>
                         <Title size={TitleSize.EXTRA_SMALL} marginBottom={24}>Сделать заказ</Title>
